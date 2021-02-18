5ed9127b4679b4ac4d4c826710cb0ccd4a4d9b84
Paula Rösler <paularoesler@web.de>
2021-02-18 14:07:54 +0100

    Add some more styling

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,37 @@
 import './Card.css'
+import { useState } from 'react' // { useState } ist eine Abkürzung für React.useState, siehe weiter unten
 
-export default function Card({ name, species, gender, status, image }) {
+export default function Card({
+  name,
+  species,
+  gender,
+  status,
+  origin,
+  location,
+  image,
+}) {
+  const [isCardDetailsVisible, setIsCardDetailsVisible] = useState(false) // siehe Kommentar oben Z 2
+
   return (
     <section className="Card">
       <h1 className="Card__title">{name}</h1>
-      <ul className="Card__details">
+      <button
+        className="Card__detailsButton"
+        onClick={event => {
+          event.stopPropagation()
+          setIsCardDetailsVisible(!isCardDetailsVisible)
+        }}
+      >
+        {!isCardDetailsVisible ? 'Show details' : 'Hide details'}
+      </button>
+      <ul hidden={!isCardDetailsVisible} className="Card__details">
         <li>Species: {species === 'Human' ? '😎' : '👽'}</li>
         <li>Gender: {gender}</li>
         <li>Status: {status}</li>
+        <li>Origin: {origin}</li>
+        <li>Location: {location}</li>
       </ul>
-      <img src={image} alt="" />
+      <img src={image} alt="" className="Card__profilePic" />
     </section>
   )
 }
