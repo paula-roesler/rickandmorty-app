f38f6fee20904a0a5ffb0f4917abd40fe41a0107
Paula Rösler <paularoesler@web.de>
2021-02-19 16:53:13 +0100

    Add FontAwsome and  Navigation icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import '../../global.css'
 import Header from '../Header'
 import CharactersPage from '../CharactersPage'
 import Navigation from '../Navigation'
-// import Card from '../Card'
 
 function App() {
   return (
@@ -12,21 +10,6 @@ function App() {
       <Header title="Rick & Morty" />
       <main>
         <CharactersPage />
-        {/* {characters.map(
-        ({ name, species, gender, status, origin, location, image, id }) => (
-          <Card
-            key={id}
-            name={name}
-            species={species}
-            gender={gender}
-            status={status}
-            origin={origin.name}
-            location={location.name}
-            buttonText="Show details"
-            image={image}
-          />
-        )
-      )} */}
       </main>
       <Navigation title="Navigation" />
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,21 @@
 import './Navigation.css'
+import { ReactComponent as Home } from '@fortawesome/fontawesome-free/svgs/solid/brain.svg'
+import { ReactComponent as Search } from '@fortawesome/fontawesome-free/svgs/solid/bullseye.svg'
+import { ReactComponent as Like } from '@fortawesome/fontawesome-free/svgs/solid/heart.svg'
+import { ReactComponent as Create } from '@fortawesome/fontawesome-free/svgs/solid/bolt.svg'
 
-export default function Navigation({ title }) {
+export default function Navigation({ onclick }) {
+  const home = <Home />
+  const search = <Search />
+  const like = <Like />
+  const create = <Create />
+
   return (
     <nav className="Navigation">
-      <h1>{title}</h1>
+      <Home className="Navigation__button" onNavigate={onclick} />
+      <Search className="Navigation__button" onNavigate={onclick} />
+      <Like className="Navigation__button" onNavigate={onclick} />
+      <Create className="Navigation__button" onNavigate={onclick} />
     </nav>
   )
 }
