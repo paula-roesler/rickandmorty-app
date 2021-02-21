d9ec4308c125dee7c404af7c7cfe8325b07802e6
Paula Rösler <paularoesler@web.de>
2021-02-21 06:42:20 +0100

    Rearrange app structure - removed app.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './global.css'
-import App from './components/App'
+import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
