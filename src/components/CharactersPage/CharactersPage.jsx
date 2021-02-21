8c967cb6fc2d7267817d944729ef2cff7c9ac5d7
Paula Rösler <paularoesler@web.de>
2021-02-21 16:19:43 +0100

    Improve Card CSS

--- src/components/CharactersPage/CharactersPage.jsx
+++ src/components/CharactersPage/CharactersPage.jsx
@@ -6,21 +6,17 @@ export default function CharactersPage() {
   const [characters, setCharacters] = useState([])
 
   useEffect(() => {
-    getAllCharacters() // no url provided, so the default parameter is used
+    getAllCharacters()
   }, [])
 
-  // url has a default value that is used in the useEffect() above
   function getAllCharacters(url = 'https://rickandmortyapi.com/api/character') {
     fetch(url)
       .then(res => res.json())
       .then(data => {
-        // data is this object: {info: ..., results: ...}
-
-        // https://reactjs.org/docs/hooks-reference.html#functional-updates
         setCharacters(oldState => [...oldState, ...data.results])
 
-        const nextUrl = data.info.next // contains the next url, if it exists
-        nextUrl && getAllCharacters(nextUrl) // if nextUrl exists, fetch it
+        const nextUrl = data.info.next
+        nextUrl && getAllCharacters(nextUrl)
       })
   }
 
